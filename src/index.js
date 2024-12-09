8769cdaf8642bb95a5e8eaacfc2cd475ceb30a6e
priyanshupande <priyanshupande23@gmail.com>
2024-12-09 13:30:04 +0530

    solve error in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import ReactDom from "react-dom/client"
-// import ".index.css"
+import ReactDOM from "react-dom";
 import App from "./App";
-// import { reportTypeError } from "ajv/dist/compile/validate/dataType";
 
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
